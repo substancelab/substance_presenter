2d8090c5b6f05cd36fbf1df04b3cde3c4de86a4f
Ole Palm <olepalm@hotmail.com>
2018-05-10 10:58:06 +0200

    Namespace Substance::Presenter
    - to avoid clashing with similar classes

--- lib/substance_presenter.rb
+++ lib/substance_presenter.rb
@@ -1,52 +1,54 @@
 # frozen_string_literal: true
 
-class Presenter < SimpleDelegator
-  attr_reader :view
+module Substance
+  class Presenter < SimpleDelegator
+    attr_reader :view
 
-  class << self
-    # Returns true if object has been wrapped in a Presenter
-    def decorated?(object)
-      object.is_a?(Presenter)
-    end
+    class << self
+      # Returns true if object has been wrapped in a Presenter
+      def decorated?(object)
+        object.is_a?(Presenter)
+      end
 
-    # Wrap object in presenter with access to a view context
-    def present(object, view)
-      return object if decorated?(object)
-      new(object, view)
-    end
+      # Wrap object in presenter with access to a view context
+      def present(object, view)
+        return object if decorated?(object)
+        new(object, view)
+      end
 
-    # Wrap each object in presenter with access to a view context
-    def present_collection(collection, view)
-      collection.map do |item|
-        present(item, view)
+      # Wrap each object in presenter with access to a view context
+      def present_collection(collection, view)
+        collection.map do |item|
+          present(item, view)
+        end
       end
     end
-  end
 
-  def initialize(object, view)
-    super(object)
-    @view = view
-  end
+    def initialize(object, view)
+      super(object)
+      @view = view
+    end
 
-  def object
-    undecorated(__getobj__)
-  end
+    def object
+      undecorated(__getobj__)
+    end
 
-  private
+    private
 
-  def decorated?(object)
-    self.class.decorated?(object)
-  end
+    def decorated?(object)
+      self.class.decorated?(object)
+    end
 
-  def present_collection(collection, presenter)
-    presenter.present_collection(collection, view)
-  end
+    def present_collection(collection, presenter)
+      presenter.present_collection(collection, view)
+    end
 
-  def undecorated(potentially_decorated_object)
-    if decorated?(potentially_decorated_object)
-      potentially_decorated_object.object
-    else
-      potentially_decorated_object
+    def undecorated(potentially_decorated_object)
+      if decorated?(potentially_decorated_object)
+        potentially_decorated_object.object
+      else
+        potentially_decorated_object
+      end
     end
   end
 end
